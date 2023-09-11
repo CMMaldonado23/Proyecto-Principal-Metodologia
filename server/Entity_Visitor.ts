1abac6b7ae8c505f808fe7ffacb6f150b01836cd
Eliana Montiel <elianamontiel_10@hotmail.com>
2023-09-11 20:18:19 -0300

    Se crearon las entidades Visitante y Categoria

---
+++ server/Entity_Visitor.ts
@@ -0,0 +1,20 @@
+class vistor {
+    private id: string
+    private ip: string
+    private nickname: string 
+
+
+    private constructor(
+        id: string,
+        ip: string,
+        nicknmame: string,
+    ){
+
+        this.id = id;
+        this.ip = ip;
+        this.nickname = nicknmame;
+
+    }
+}
+    export default vistor;
+
