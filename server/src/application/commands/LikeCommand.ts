e42d403a27141bac59af9af19377fa10eee80323
Juan Cruz Lescano <juancruzlescano@hotmail.com>
2023-10-30 21:37:34 -0300

    LikeCommand creado

---
+++ server/src/application/commands/LikeCommand.ts
@@ -0,0 +1,26 @@
+class CreateDislikeCommand {
+    private readonly visitorId: string;
+    private readonly visitorPin: string;
+    private readonly claimId: string;
+
+    public constructor(
+        visitorId: string, 
+        visitorPin: string,
+        claimId: string,     
+        ){
+            this.visitorId = visitorId;
+            this.visitorPin = visitorPin;
+            this.claimId = claimId;
+        }
+
+    public getvisitorId(): string {
+        return this.visitorId;
+    }    
+    public getvisitorPin(): string {
+        return this.visitorPin;
+    }
+    public getclaimId(): string {
+        return this.claimId;
+    }
+}
+export default CreateDislikeCommand;
