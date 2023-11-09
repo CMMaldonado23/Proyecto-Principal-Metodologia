9a3b9033240e7425e899150a813f13ef82bab298
Juan Cruz Lescano <juancruzlescano@hotmail.com>
2023-11-08 22:27:35 -0300

    CorreccionDeActions

--- server/src/http/actions/CreateVisitorAction.ts
+++ server/src/http/actions/CreateVisitorAction.ts
@@ -1,22 +1,26 @@
 import { Request, Response } from 'express';
-import CreateVisitorCommand from '../../../application/commands/visitor/create.visitor.command';
-import createVisitorHandler from '../../../application/handlers/visitor/create.visitor.handler';
+import CreateVisitorCommand from 'application/commands/create.visitor.command';
+import createVisitorHandler from 'application/handlers/create.visitor.handler';
 
 class CreateVisitorAction {
   public async run(req: Request, res: Response) {
-    const { id, ip, nickname } = req.body;
-
+    const { id, ip, nickname,pin } = req.body;
+    
     try {
+      if (!ip || !nickname || !pin) {
+        res.status(400).json({message: "Todos los campos son obligatorios"});
+        return}
       const command = new CreateVisitorCommand(
         id,
         ip,
-        nickname
+        nickname,
+        pin,
       );
       
       await createVisitorHandler.execute(command);
 
       return res.status(201).json(
-        { message: 'Visitor created successfully' }
+        { message: 'Creacion exitosa' }
       );
     } catch (error) {
       res.status(400).json(
