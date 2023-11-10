85313b1c49f7ecc9d8c713a59e17f7d032bbaa68
Juan Cruz Lescano <juancruzlescano@hotmail.com>
2023-11-10 10:41:48 -0300

    CorreccionRutasClaim

--- server/src/http/routes/rutasClaim.ts
+++ server/src/http/routes/rutasClaim.ts
@@ -1,8 +1,11 @@
 import { Application } from 'express';
 import CommonRoutes from './common.routes';
-import createClaimAction from '../actions/claim/create.claim.action';
-import updateClaimAction from '../actions/claim/update.claim.action';
-import findClaimAction from '../actions/claim/find.claim.action';
+import createClaimAction from 'http/actions/CreateClaimAction';
+import likeClaimAction from 'http/actions/LikeAction';
+import reportDuplicateClaimAction from 'http/actions/report.claim.action';
+import getLastClaimsAction from 'http/actions/GetLastClaimsAction';
+import findLastFiveClaimsAction from 'http/actions/find.last.five.Onfire.claim.action';
+
 
 class ClaimRoutes extends CommonRoutes {
   public constructor(app: Application) {
@@ -11,8 +14,10 @@ class ClaimRoutes extends CommonRoutes {
 
   public setUpRoutes(): Application {
     this.app.post('/claim', createClaimAction.run);
-    this.app.get('/claim', findClaimAction.run);
-    this.app.put('/claim/:id', updateClaimAction.run);
+    this.app.put('/claims/:id/like', likeClaimAction.run);
+    this.app.put('/claims/:id/report', reportDuplicateClaimAction.run)
+    this.app.get('/claims', getLastClaimsAction.run);
+    this.app.get('/claims/on-fire', findLastFiveClaimsAction.run);
 
     return this.app;
   }
