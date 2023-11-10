19ca731c5ad427c56208d8b80e53d3a264fa0bc3
Juan Cruz Lescano <juancruzlescano@hotmail.com>
2023-11-10 12:07:41 -0300

    RutasVisitorCorregidas

--- server/src/http/routes/Routes_visitor.ts
+++ server/src/http/routes/Routes_visitor.ts
@@ -1,8 +1,8 @@
 import { Application } from 'express';
 import CommonRoutes from './common.routes';
-import createVisitorAction from '../actions/visitor/create.visitor.action';
-import updateVisitorAction from '../actions/visitor/update.visitor.action';
-import findVisitorAction from '../actions/visitor/find.visitor.action';
+import createVisitorAction from 'http/actions/CreateVisitorAction';
+import updateVisitorAction from 'http/actions/update.visitor.action';
+import findVisitorAction from 'http/actions/find.visitor.action';
 
 
 
@@ -17,8 +17,6 @@ public setUpRoutes(): Application{
     this.app.post('/visitor', createVisitorAction.run);
     this.app.get('/visitor', findVisitorAction.run);
     this.app.put('/visitor/:id', updateVisitorAction.Run);
-
-
     return this.app;
  }
 }
