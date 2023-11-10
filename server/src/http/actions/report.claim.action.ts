aed33993009185d56a3fae7ccdab81de5fcb6c7f
Matias Maldonado <katamaldonado03@gmail.com>
2023-11-10 03:10:56 -0300

    Agrege el report claim action, porque el merge estaba vacio

---
+++ server/src/http/actions/report.claim.action.ts
@@ -0,0 +1,32 @@
+import {Request, Response} from 'express';
+import {ReportDuplicateClaimCommand} from "../../application/commands/reportDuplicate.claim.command";
+import reportClaimHandler, {ReportClaimHandler} from "../../application/handlers/ReportClaimHandler";
+
+export class reportClaimAction{
+    constructor(
+        private handler: reportClaimHandler
+    ){        
+    }
+
+    public async run (req: Request, res: Response): Promise<void>{
+        const {id} = req.params;
+        const {originalId} = req.body;
+
+
+        if(!originalId || id){
+            res.status(400).json({message: 'Es requerido el id original '});
+            return;
+        }
+
+        try{
+            const command = new ReportDuplicateClaimCommand(id,originalId);
+            await this.handler.handle(command);
+
+            res.status(200).json({message: 'Reclamo reportado'})
+        } catch(e:any){
+            res.status(400).json({message: e.message})
+        }
+    }
+}
+
+export default new reportClaimAction(reportClaimHandler)
